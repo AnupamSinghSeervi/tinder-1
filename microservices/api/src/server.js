eacdda4e92ae00fcc58b31590035b1c7997b1130
Anupam Singh <anupamwants@gmail.com>
2018-02-05 17:55:06 +0530

    Added hasura auth (signup) api

--- microservices/api/src/server.js
+++ microservices/api/src/server.js
@@ -18,7 +18,37 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use('/', hasuraExamplesRouter);
+var fetchAction =  require('node-fetch');
+// auth api from hasura
+var url = "https://auth.gamey34.hasura-app.io/v1/signup";
 
+var requestOptions = {
+    "method": "POST",
+    "headers": {
+        "Content-Type": "application/json"
+    }
+};
+
+var body = {
+    "provider": "username",
+    "data": {
+        "username": "johnsmith",
+        "password": "js@hasura"
+    }
+};
+
+requestOptions.body = JSON.stringify(body);
+
+fetchAction(url, requestOptions)
+.then(function(response) {
+	return response.json();
+})
+.then(function(result) {
+	console.log(JSON.stringify(result));
+})
+.catch(function(error) {
+	console.log('Request Failed:' + error);
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
